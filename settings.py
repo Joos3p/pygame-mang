ef957cd1df1539f9c05696372aebb1fb76b591de
Joosep <146734620+Joos3p@users.noreply.github.com>
2025-03-25 14:08:07 +0200

    settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    
+    def __init__(self):
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (255, 14, 216)
+        self.caption = "Bubble Bluster"
+        
+        self.bubble_min_r = 10
+        self.bubble_max_r = 50
+        
